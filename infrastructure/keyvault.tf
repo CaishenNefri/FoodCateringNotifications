722ab43e4d77d0137426a581e6f347abfab58f80
Caishen <24476768+CaishenNefri@users.noreply.github.com>
2023-12-02 23:04:42 +0100

    Create secrets from terraform from tfvars not commited

---
+++ infrastructure/keyvault.tf
@@ -0,0 +1,82 @@
+resource "azurerm_key_vault" "example" {
+  name                        = "kvzc"
+  resource_group_name         = azurerm_resource_group.example.name
+  location                    = azurerm_resource_group.example.location
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "identity" {
+  key_vault_id = azurerm_key_vault.example.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_user_assigned_identity.example.principal_id
+
+  key_permissions = [
+    "Get",
+    "List",
+    "Create",
+    "Delete",
+    "Backup",
+    "Restore"
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Backup",
+    "Restore"
+  ]
+}
+
+
+resource "azurerm_key_vault_access_policy" "me" {
+  key_vault_id = azurerm_key_vault.example.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+    "List",
+    "Create",
+    "Delete",
+    "Backup",
+    "Restore"
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Backup",
+    "Restore"
+  ]
+}
+
+
+resource "azurerm_key_vault_secret" "telegram_token" {
+  name         = "telegram-token"
+  value        = var.telegram_token
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azurerm_key_vault_secret" "telegram_chat_id" {
+  name         = "telegram-chat-id"
+  value        = var.telegram_chat_id
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azurerm_key_vault_secret" "zc_username" {
+  name         = "zc-username"
+  value        = var.zc_username
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azurerm_key_vault_secret" "zc_password" {
+  name         = "zc-password"
+  value        = var.zc_password
+  key_vault_id = azurerm_key_vault.example.id
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,67 +12,6 @@ resource "azurerm_user_assigned_identity" "example" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
-
-resource "azurerm_key_vault" "example" {
-  name                        = "kvzc"
-  resource_group_name         = azurerm_resource_group.example.name
-  location                    = azurerm_resource_group.example.location
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-}
-
-resource "azurerm_key_vault_access_policy" "identity" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_user_assigned_identity.example.principal_id
-
-  key_permissions = [
-    "Get",
-    "List",
-    "Create",
-    "Delete",
-    "Backup",
-    "Restore"
-  ]
-
-  secret_permissions = [
-    "Get",
-    "List",
-    "Set",
-    "Delete",
-    "Backup",
-    "Restore"
-  ]
-}
-
-
-resource "azurerm_key_vault_access_policy" "me" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  key_permissions = [
-    "Get",
-    "List",
-    "Create",
-    "Delete",
-    "Backup",
-    "Restore"
-  ]
-
-  secret_permissions = [
-    "Get",
-    "List",
-    "Set",
-    "Delete",
-    "Backup",
-    "Restore"
-  ]
-}
-
-
-
 # Container App
 resource "azurerm_container_app_environment" "example" {
   name                = "Example-Environment"
