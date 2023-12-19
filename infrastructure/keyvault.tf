aa3cb140b6dd6fe46826660e3d5582dc13c65c16
Caishen <24476768+CaishenNefri@users.noreply.github.com>
2023-12-19 10:36:37 +0100

    Import kv secrets because RG was removed but kv is soft deleted

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -31,6 +31,10 @@ resource "azurerm_key_vault_access_policy" "identity" {
   ]
 }
 
+import {
+  to = azurerm_key_vault_access_policy.me
+  id = "/subscriptions/1564f82f-9f08-47e0-9939-5e3dcc739b5e/resourceGroups/zdrowycatering/providers/Microsoft.KeyVault/vaults/kvzc/objectId/e3c0b5df-3a0d-4d56-b200-c44903c48bc9"
+}
 
 resource "azurerm_key_vault_access_policy" "me" {
   key_vault_id = azurerm_key_vault.example.id
@@ -57,11 +61,20 @@ resource "azurerm_key_vault_access_policy" "me" {
   ]
 }
 
+import {
+  to = azurerm_key_vault_secret.telegram_token
+  id = "https://kvzc.vault.azure.net/secrets/telegram-token/a437bde813324883a33852945bd79f16"
+}
 
 resource "azurerm_key_vault_secret" "telegram_token" {
   name         = "telegram-token"
   value        = var.telegram_token
   key_vault_id = azurerm_key_vault.example.id
+}
+
+import {
+  to = azurerm_key_vault_secret.telegram_chat_id
+  id = "https://kvzc.vault.azure.net/secrets/telegram-chat-id/e9822ce0fb014fe58874d58b687ed3ac"
 }
 
 resource "azurerm_key_vault_secret" "telegram_chat_id" {
@@ -70,10 +83,20 @@ resource "azurerm_key_vault_secret" "telegram_chat_id" {
   key_vault_id = azurerm_key_vault.example.id
 }
 
+import {
+  to = azurerm_key_vault_secret.zc_username
+  id = "https://kvzc.vault.azure.net/secrets/zc-username/1d7662de16d14f91a9fb0b6598982021"
+}
+
 resource "azurerm_key_vault_secret" "zc_username" {
   name         = "zc-username"
   value        = var.zc_username
   key_vault_id = azurerm_key_vault.example.id
+}
+
+import {
+  to = azurerm_key_vault_secret.zc_password
+  id = "https://kvzc.vault.azure.net/secrets/zc-password/cf131a75172f4081bfd9b05331ef7e46"
 }
 
 resource "azurerm_key_vault_secret" "zc_password" {
